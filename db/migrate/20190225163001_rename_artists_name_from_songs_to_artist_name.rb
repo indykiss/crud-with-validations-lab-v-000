e63235f9b41320ac88b3fb71d3cc71469439d57f
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-25 16:31:19 +0000

    Automatically backed up by Learn

--- db/migrate/20190225163001_rename_artists_name_from_songs_to_artist_name.rb
+++ db/migrate/20190225163001_rename_artists_name_from_songs_to_artist_name.rb
@@ -1,5 +1,5 @@
 class RenameArtistsNameFromSongsToArtistName < ActiveRecord::Migration
   def change
-    rename_column :song, :artists_name, :artist_name
+    rename_column :songs, :artists_name, :artist_name
   end
 end
