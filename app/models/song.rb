a91fca5ba891033855bc18e60bf2a2cb834776ed
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-25 16:28:18 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true,
-  validates :title, uniqueness: true 
+  validates :title, presence: true
+  validates :title, uniqueness: true
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
   validate :release_yr
