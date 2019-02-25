0cf4f614b01cc4ac9fd11e0394e670e6d55cca8e
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-25 16:36:09 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,5 +4,5 @@ class Song < ActiveRecord::Base
   validates :release_year, presence: true, if: :released, presence: true
   validates :release_year, numericality: {less_than_or_equal_to: Time.now.year}, allow_nil: true
 
-  
+
 end
