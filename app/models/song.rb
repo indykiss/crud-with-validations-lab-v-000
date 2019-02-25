96715746ad49027ac9909c2fb0eac294eebbf279
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-25 16:36:05 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,17 +1,8 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: true
-  validates :released, inclusion: { in: [true, false] }
-  validates :artist_name, presence: true
-  validate :release_yr
-  validates_numericality_of :release_year, :only_integer => true, :less_than_or_equal_to => Date.current.year, allow_nil: true
+  validates :release_year, presence: true, if: :released, presence: true
+  validates :release_year, numericality: {less_than_or_equal_to: Time.now.year}, allow_nil: true
 
-  def release_yr
-    if self.released == true
-        unless !!self.release_year
-            errors.add(:release_year, "must define release year")
-        end
-
-    end
-  end
+  
 end
